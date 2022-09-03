b272e469d122d8c5f1c25278b035bae1555c0605
Alberto Gabriel Canda Hernández <110306454+AlbertG590@users.noreply.github.com>
2022-09-03 14:45:24 -0600

    Add files via upload

---
+++ I Examen Parcial/src/Main/conversorDeBytes.java
@@ -0,0 +1,28 @@
+package Main;
+
+import java.util.Scanner;
+
+public class conversorDeBytes {
+
+	public conversorDeBytes() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println ("BIenvenido al conversor de Bytes");
+		int bytes = 1;
+		int kilobytes = 1000;
+		int megabytes = 1024;
+		
+		//1 Convertir bytes a kilobytes
+		//1/1000
+		
+		//2. Convertit Kilobytes a Megabytes
+		//1000/1024
+		
+		//3. Salir
+
+	}
+
+}
